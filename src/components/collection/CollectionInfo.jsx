a1f4210cae7ccae76b211c25cd231b2d860ae1e4
Rmljax <ryan.lockenbach@gmail.com>
2026-03-23 12:40:04 -0400

    dynamic collection page

--- src/components/collection/CollectionInfo.jsx
+++ src/components/collection/CollectionInfo.jsx
@@ -1,37 +1,75 @@
 import React from "react";
+import Skeleton from "../ui/Skeleton";
 
-export default function CollectionInfo() {
+export default function CollectionInfo({ collection }) {
   return (
     <section id="collection-info">
       <div className="row">
-        <div className="collection-info__wrapper">
-          <p className="collection-info__description">
-            The Meebits are 20,000 unique 3D voxel characters, created by a
-            custom generative algorithm, then registered on the Ethereum
-            blockchain.
-          </p>
-          <div className="collection-info__details">
-            <span className="collection-info__detail">
-              Items
-              <span className="collection-info__detail__data"> 20K</span>
-            </span>
-            ·
-            <span className="collection-info__detail">
-              Created
-              <span className="collection-info__detail__data"> May 2021</span>
-            </span>
-            ·
-            <span className="collection-info__detail">
-              Creator earnings
-              <span className="collection-info__detail__data"> 1%</span>
-            </span>
-            ·
-            <span className="collection-info__detail">
-              Chain
-              <span className="collection-info__detail__data"> Ethereum</span>
-            </span>
+        {Object.keys(collection).length ? (
+          <div className="collection-info__wrapper">
+            <p className="collection-info__description">
+              {collection.description}
+            </p>
+            <div className="collection-info__details">
+              <span className="collection-info__detail">
+                Items
+                <span className="collection-info__detail__data">
+                  {" "}
+                  {collection?.items?.length}
+                </span>
+              </span>
+              ·
+              <span className="collection-info__detail">
+                Created
+                <span className="collection-info__detail__data">
+                  {" "}
+                  {collection.createdDate}
+                </span>
+              </span>
+              ·
+              <span className="collection-info__detail">
+                Creator earnings
+                <span className="collection-info__detail__data">
+                  {" "}
+                  {collection.creatorEarnings}%
+                </span>
+              </span>
+              ·
+              <span className="collection-info__detail">
+                Chain
+                <span className="collection-info__detail__data">
+                  {" "}
+                  {collection.chain}
+                </span>
+              </span>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="collection-info__wrapper">
+            <div className="collection-info__description">
+              <Skeleton width="100%" height="16px" borderRadius="4px" />
+              <Skeleton width="100%" height="16px" borderRadius="4px" />
+              <Skeleton width="70%" height="16px" borderRadius="4px" />
+            </div>
+            <div className="collection-info__details">
+              <span className="collection-info__detail">
+                <Skeleton width="58px" height="16px" borderRadius="4px" />
+              </span>
+              ·
+              <span className="collection-info__detail">
+                <Skeleton width="120px" height="16px" borderRadius="4px" />
+              </span>
+              ·
+              <span className="collection-info__detail">
+                <Skeleton width="132px" height="16px" borderRadius="4px" />
+              </span>
+              ·
+              <span className="collection-info__detail">
+                <Skeleton width="108px" height="16px" borderRadius="4px" />
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
